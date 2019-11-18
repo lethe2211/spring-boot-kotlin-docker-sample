16dc5604612db90a280fa75fd86a502f36a0d13d
lethe2211 <lethe2211@gmail.com>
2019-11-18 15:54:12 +0900

    Add a dependent API

--- webapp/src/main/kotlin/com/example/demo/controller/DemoController.kt
+++ webapp/src/main/kotlin/com/example/demo/controller/DemoController.kt
@@ -1,14 +1,33 @@
 package com.example.demo.controller
 
+import com.example.demo.domain.Response
+import com.example.demo.service.DependentService
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class DemoController {
+class DemoController(
+    private val dependentService: DependentService
+) {
 
     @GetMapping(value = ["/"])
     fun root(): ResponseEntity<String> {
         return ResponseEntity.ok("OK")
+    }
+
+    @GetMapping(value = ["/call-dependent-api"])
+    fun callDependentAPI(): ResponseEntity<Response> {
+        val result = dependentService.callDependentAPI()
+        logger.info(result.toString())
+        return ResponseEntity.ok(
+            Response(status = result.status)
+        )
+    }
+
+    companion object {
+        val logger: Logger = LoggerFactory.getLogger(this::class.java)
     }
 }
